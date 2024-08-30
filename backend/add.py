00fd0cb478732d1604de298a40691448f0872548
Rotelanin <egorij0107up2@gmail.com>
2024-08-31 01:31:17 +0300

    added direct template

--- backend/add.py
+++ backend/add.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template_string("""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    </head>
+    <body>
+    <h1>Hello, World!</h1>
+    </body>
+    </html>
+    """)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
